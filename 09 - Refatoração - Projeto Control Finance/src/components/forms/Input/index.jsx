503421851a3c0889c6a37f1a417e60947d7df0fd
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-03-25 12:27:45 -0300

    added m5-t22-sp3-demo3-desafio-resolucao

--- 09 - Refatoração - Projeto Control Finance/src/components/forms/Input/index.jsx
+++ 09 - Refatoração - Projeto Control Finance/src/components/forms/Input/index.jsx
@@ -10,7 +10,17 @@ export const Input = ({ setValue, label, id, type, placeholder, value }) => {
         type={type}
         placeholder={placeholder}
         value={value}
-        onChange={(event) => setValue(event.target.valueAsNumber)}
+        // onChange={(event) => setValue(event.target.valueAsNumber)}
+        /*
+          O input é do tipo number ? CASO SIM : CASO NAO
+            SIM -> Já faz a converção do value para number (valueAsNumber)
+            NÃO -> Utiliza o valor do input em string
+        */
+        onChange={(event) =>
+          setValue(
+            type === "number" ? event.target.valueAsNumber : event.target.value
+          )
+        }
       />
     </div>
   );
